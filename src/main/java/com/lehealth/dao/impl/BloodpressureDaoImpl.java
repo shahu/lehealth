58230b5d95bcce5361675637a9232cb07280994a
ryl1990 <ryl1990@126.com>
2014-10-25 19:03:09 +0800

    1

--- src/main/java/com/lehealth/dao/impl/BloodpressureDaoImpl.java
+++ src/main/java/com/lehealth/dao/impl/BloodpressureDaoImpl.java
@@ -17,7 +17,7 @@ public class BloodpressureDaoImpl extends BaseJdbcDao implements BloodpressureDa
 
 	@Override
 	public List<BloodpressureInfo> selectBloodpressureRecords(String userId) {
-		String sql="SELECT * FROM BpRecords WHERE userid=:userid ORDER BY recordtime DESC limit 7";
+		String sql="SELECT * FROM BpRecords WHERE userid=:userid ORDER BY recordDate DESC limit 7";
 		MapSqlParameterSource msps=new MapSqlParameterSource();
 		msps.addValue("userid", userId);
 		List<BloodpressureInfo> list = new ArrayList<BloodpressureInfo>();

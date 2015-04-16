652a668c3a538f92c818f366fea65bda42df54f9
yefeizhang <yefeizhang@pptv.com>
2015-04-16 23:43:11 +0800

    add sha1

--- src/main/webapp/js/example/lehealth/common.js
+++ src/main/webapp/js/example/lehealth/common.js
@@ -104,27 +104,110 @@ define(function(require, exports, module) {
 	};
 
 	exports.arrayEqual = function(arr1, arr2) {
-		if(arr1 && arr2) {
-			if(arr1.length == arr2.length) {
-				for(var i = 0; i < arr1.length; i++) {
+		if (arr1 && arr2) {
+			if (arr1.length == arr2.length) {
+				for (var i = 0; i < arr1.length; i++) {
 					var found = false;
-					for(var j = 0; j < arr2.length; j++) {
-						if(arr1[i] == arr2[j]) {
+					for (var j = 0; j < arr2.length; j++) {
+						if (arr1[i] == arr2[j]) {
 							found = true;
 						}
 					}
-					if(!found) {
+					if (!found) {
 						return false;
 					}
 				}
 				return true;
 			}
-		} else if(!arr1 && !arr2) {
+		} else if (!arr1 && !arr2) {
 			return true;
 		} else {
 			return false;
 		}
 		return false;
+	};
+
+	exports.sha1 = function(str) {
+		// 对str进行UTF-8编码，转成Uint8Array
+		var tmp = new Uint8Array(encodeUTF8(str));
+		// sha1加密
+		var result = encodeSHA1(tmp);
+		// Uint8Array转回str
+		return Array.prototype.map.call(result, function(e) {
+			return (e < 16 ? "0" : "") + e.toString(16);
+		}).join("");
+
+		function encodeUTF8(s) {
+			var i, r = [],
+				c, x;
+			for (i = 0; i < s.length; i++) {
+				if ((c = s.charCodeAt(i)) < 0x80) {
+					r.push(c);
+				} else if (c < 0x800) {
+					r.push(0xC0 + (c >> 6 & 0x1F), 0x80 + (c & 0x3F));
+				} else {
+					if ((x = c ^ 0xD800) >> 10 == 0) {
+						c = (x << 10) + (s.charCodeAt(++i) ^ 0xDC00) + 0x10000;
+						r.push(0xF0 + (c >> 18 & 0x7), 0x80 + (c >> 12 & 0x3F));
+					} else {
+						r.push(0xE0 + (c >> 12 & 0xF));
+					}
+					r.push(0x80 + (c >> 6 & 0x3F), 0x80 + (c & 0x3F));
+				};
+			}
+			return r;
+		}
+
+		function encodeSHA1(data) {
+			var i, j, t;
+			var l = ((data.length + 8) >>> 6 << 4) + 16,
+				s = new Uint8Array(l << 2);
+			s.set(new Uint8Array(data.buffer)), s = new Uint32Array(s.buffer);
+			for (t = new DataView(s.buffer), i = 0; i < l; i++) {
+				s[i] = t.getUint32(i << 2);
+			}
+			s[data.length >> 2] |= 0x80 << (24 - (data.length & 3) * 8);
+			s[l - 1] = data.length << 3;
+			var w = [];
+			var f = [
+
+				function() {
+					return m[1] & m[2] | ~m[1] & m[3];
+				},
+				function() {
+					return m[1] ^ m[2] ^ m[3];
+				},
+				function() {
+					return m[1] & m[2] | m[1] & m[3] | m[2] & m[3];
+				},
+				function() {
+					return m[1] ^ m[2] ^ m[3];
+				}
+			];
+			var rol = function(n, c) {
+				return n << c | n >>> (32 - c);
+			};
+			var k = [1518500249, 1859775393, -1894007588, -899497514];
+			var m = [1732584193, -271733879, null, null, -1009589776];
+			m[2] = ~m[0];
+			m[3] = ~m[1];
+			for (i = 0; i < s.length; i += 16) {
+				var o = m.slice(0);
+				for (j = 0; j < 80; j++) {
+					w[j] = j < 16 ? s[i + j] : rol(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1);
+					t = rol(m[0], 5) + f[j / 20 | 0]() + m[4] + w[j] + k[j / 20 | 0] | 0;
+					m[1] = rol(m[1], 30), m.pop(), m.unshift(t);
+				}
+				for (j = 0; j < 5; j++) {
+					m[j] = m[j] + o[j] | 0;
+				}
+			};
+			t = new DataView(new Uint32Array(m).buffer);
+			for (var i = 0; i < 5; i++) {
+				m[i] = t.getUint32(i << 2);
+			}
+			return new Uint8Array(new Uint32Array(m).buffer);
+		}
 	};
 
 });
